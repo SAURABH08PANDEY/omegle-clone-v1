f8f34c7347ded9cf64bf3062957f649df160725c
SAURABH PANDEY <80577220+SAURABH08PANDEY@users.noreply.github.com>
2023-09-12 01:11:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
-const connectDB = require('./db/connectDB');
-const express = require('express');
-const path = require('path');
+const connectDB = require("./db/connectDB");
+const express = require("express");
+const path = require("path");
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const port = process.env.PORT || 4000;
-const {v4:uuidv4} = require('uuid');
-const {ExpressPeerServer} = require('peer')
+const { v4: uuidv4 } = require("uuid");
+const { ExpressPeerServer } = require("peer");
 const { allocate, deallocate } = require("./controller/allocateRoom");
 require("dotenv").config();
-const peer = ExpressPeerServer(server , {
-  debug:true
+const peer = ExpressPeerServer(server, {
+  debug: true,
 });
 
-app.use('/peerjs', peer);
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static('public'))
-app.get('/' , async(req, res) => {
-  data = await allocate();
-  return res.render('index' , { RoomId: data })
+app.use("/peerjs", peer);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.json({ message: "go to /start!!!" });
 });
-app.get('*',(req, res) => {
-  res.json({error: "no such route was found!!!"})
-})
-io.on("connection" , (socket)=>{
-  socket.on('newUser' , (id , room)=>{
-    console.log(room, '-----------------------');
+app.get("/start", async (req, res) => {
+  data = await allocate();
+  return res.render("index", { RoomId: data });
+});
+app.get("*", (req, res) => {
+  res.json({ error: "no such route was found!!!" });
+});
+io.on("connection", (socket) => {
+  socket.on("newUser", (id, room) => {
+    console.log(room, "-----------------------");
     socket.join(room);
-    socket.to(room).broadcast.emit('userJoined' , id);
-    socket.on('disconnect' , ()=>{
+    socket.to(room).broadcast.emit("userJoined", id);
+    socket.on("disconnect", () => {
       console.log(room);
-        deallocate(room);
-        socket.to(room).broadcast.emit('userDisconnect' , id);
-    })
-  })
-})
+      deallocate(room);
+      socket.to(room).broadcast.emit("userDisconnect", id);
+    });
+  });
+});
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
